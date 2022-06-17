bc5821740a6b29a95d3ab2bb53e5c0745255336e
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2022-06-17 13:22:12 +0530

    style: 💄 Resolve type errors

--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,12 +1,34 @@
+import { Params } from '@feathersjs/feathers';
 import { Service, KnexServiceOptions } from 'feathers-knex';
 import { Application } from '../../declarations';
 
+export type UsersData = {
+	id?: string;
+	email?: string;
+	username: string;
+	password: string;
+	githubId?: string;
+	createdAt: Date;
+	updatedAt: Date;
+	deletedAt: Date | null;
+};
+
 export class Users extends Service {
-  //eslint-disable-next-line @typescript-eslint/no-unused-vars
-  constructor(options: Partial<KnexServiceOptions>, app: Application) {
-    super({
-      ...options,
-      name: 'users'
-    });
-  }
+	//eslint-disable-next-line @typescript-eslint/no-unused-vars
+	constructor(options: Partial<KnexServiceOptions>, app: Application) {
+		super({
+			...options,
+			name: 'users',
+		});
+	}
+	async create(
+		data: Partial<UsersData>,
+		params?: Params | undefined
+	): Promise<UsersData> {
+		return this.options.Model.insert({
+			username: data.username,
+			password: data.password,
+			email: data.email,
+		}).into('users');
+	}
 }
--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -1,42 +1,65 @@
 import * as feathersAuthentication from '@feathersjs/authentication';
 import * as local from '@feathersjs/authentication-local';
+import { HookContext } from '../../app';
+import { UsersData } from './users.class';
 // Don't remove this comment. It's needed to format import lines nicely.
-
+import { HooksObject, Service } from '@feathersjs/feathers';
 const { authenticate } = feathersAuthentication.hooks;
 const { hashPassword, protect } = local.hooks;
 
-export default {
-  before: {
-    all: [],
-    find: [ authenticate('jwt') ],
-    get: [ authenticate('jwt') ],
-    create: [ hashPassword('password') ],
-    update: [ hashPassword('password'),  authenticate('jwt') ],
-    patch: [ hashPassword('password'),  authenticate('jwt') ],
-    remove: [ authenticate('jwt') ]
-  },
+const hooks: Partial<HooksObject<any>> = {
+	before: {
+		all: [],
+		find: [authenticate('jwt')],
+		get: [authenticate('jwt')],
+		create: [
+			hashPassword('password'),
+			(context: any) => {
+				(context.data as UsersData).updatedAt = new Date();
+				return context;
+			},
+		],
+		update: [
+			hashPassword('password'),
+			authenticate('jwt'),
+			(context: any) => {
+				(context.data as UsersData).updatedAt = new Date();
+				return context;
+			},
+		],
+		patch: [hashPassword('password'), authenticate('jwt')],
+		remove: [
+			authenticate('jwt'),
+			(context: any) => {
+				(context.data as UsersData).deletedAt = new Date();
+				return context;
+			},
+		],
+	},
 
-  after: {
-    all: [ 
-      // Make sure the password field is never sent to the client
-      // Always must be the last hook
-      protect('password')
-    ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+	after: {
+		all: [
+			// Make sure the password field is never sent to the client
+			// Always must be the last hook
+			protect('password'),
+		],
+		find: [],
+		get: [],
+		create: [],
+		update: [],
+		patch: [],
+		remove: [],
+	},
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+	error: {
+		all: [],
+		find: [],
+		get: [],
+		create: [],
+		update: [],
+		patch: [],
+		remove: [],
+	},
 };
+
+export default hooks;
